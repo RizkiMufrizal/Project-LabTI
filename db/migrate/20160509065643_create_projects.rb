b2171e885527f56d1efb44608f8560f865a881a1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-09 14:07:55 +0700

    tambah Migration projects

---
+++ db/migrate/20160509065643_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects, id: false do |t|
+      t.primary_key :id_project, :string, limit: 150
+      t.string :name_project, limit: 50
+      t.string :responsible_person, limit: 50
+      t.string :name_file, limit: 150
+
+      t.timestamps null: false
+    end
+  end
+end
