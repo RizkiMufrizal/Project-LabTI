ccea785e8c7fe128a9477d9a1b91423ef56260d0
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-23 18:33:49 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,44 +11,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_509_065_851) do
+ActiveRecord::Schema.define(version: 20160509065851) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'practicums', primary_key: 'id_practicum', force: :cascade do |t|
-    t.string   'practicum_name', limit: 50
-    t.datetime 'created_at',                null: false
-    t.datetime 'updated_at',                null: false
+  create_table "practicums", primary_key: "id_practicum", force: :cascade do |t|
+    t.string   "practicum_name", limit: 50
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  create_table 'projects', primary_key: 'id_project', force: :cascade do |t|
-    t.string   'name_project',           limit: 50
-    t.string   'responsible_person',     limit: 50
-    t.string   'name_file',              limit: 150
-    t.datetime 'created_at',                         null: false
-    t.datetime 'updated_at',                         null: false
-    t.string   'student_npm',            limit: 8
-    t.string   'practicum_id_practicum', limit: 150
+  create_table "projects", primary_key: "id_project", force: :cascade do |t|
+    t.string   "name_project",           limit: 50
+    t.string   "responsible_person",     limit: 50
+    t.string   "name_file",              limit: 150
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "student_npm",            limit: 8
+    t.string   "practicum_id_practicum", limit: 150
   end
 
-  create_table 'students', primary_key: 'npm', force: :cascade do |t|
-    t.string   'name',       limit: 50
-    t.string   'class',      limit: 6
-    t.string   'gender',     limit: 6
-    t.text     'address'
-    t.datetime 'created_at',            null: false
-    t.datetime 'updated_at',            null: false
+  create_table "students", primary_key: "npm", force: :cascade do |t|
+    t.string   "name",       limit: 50
+    t.string   "class",      limit: 6
+    t.string   "gender",     limit: 6
+    t.text     "address"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
-  create_table 'users', primary_key: 'email', force: :cascade do |t|
-    t.string   'password_hash', limit: 150
-    t.string   'password_salt', limit: 150
-    t.boolean  'enable'
-    t.string   'role', limit: 20
-    t.datetime 'created_at',                null: false
-    t.datetime 'updated_at',                null: false
+  create_table "users", primary_key: "email", force: :cascade do |t|
+    t.string   "password_hash", limit: 150
+    t.string   "password_salt", limit: 150
+    t.boolean  "enable"
+    t.string   "role",          limit: 20
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
-  add_foreign_key 'projects', 'practicums', column: 'practicum_id_practicum', primary_key: 'id_practicum', on_update: :cascade, on_delete: :cascade
-  add_foreign_key 'projects', 'students', column: 'student_npm', primary_key: 'npm', on_update: :cascade, on_delete: :cascade
+  add_foreign_key "projects", "practicums", column: "practicum_id_practicum", primary_key: "id_practicum", on_update: :cascade, on_delete: :cascade
+  add_foreign_key "projects", "students", column: "student_npm", primary_key: "npm", on_update: :cascade, on_delete: :cascade
 end
