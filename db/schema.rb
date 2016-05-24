b8127f543fa1f9b499550f47b9131150e1226635
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 21:06:30 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,16 @@ ActiveRecord::Schema.define(version: 20160509065851) do
   end
 
   create_table "students", primary_key: "npm", force: :cascade do |t|
-    t.string   "name",       limit: 50
-    t.string   "class",      limit: 6
-    t.string   "gender",     limit: 6
+    t.string   "name",          limit: 50
+    t.string   "class",         limit: 6
+    t.string   "gender",        limit: 6
     t.text     "address"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "password_hash", limit: 150
+    t.string   "password_salt", limit: 150
+    t.boolean  "enable"
+    t.string   "role",          limit: 20
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "users", primary_key: "email", force: :cascade do |t|
