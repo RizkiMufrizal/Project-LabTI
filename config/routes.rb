93c874f9aba7a96d7cb866164296b3bb7b3fea71
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 22:27:17 +0700

    tambah resources students

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'session#login'
   resources 'practicums'
+  resources 'students'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
