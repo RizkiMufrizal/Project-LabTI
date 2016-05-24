ba228343d7eb296e14c51bf2ceb7e68a892e0d4e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 21:09:32 +0700

    update model Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,13 @@
 class Student < ActiveRecord::Base
+  before_create :set_role_enable
   validates :npm, :name, :class, :gender, :address, presence: true
+
+  private
+
+  def set_role_enable
+    self.role = 'ROLE_MAHASISWA'
+    self.enable = true
+  end
 
   has_many :projects
 end
