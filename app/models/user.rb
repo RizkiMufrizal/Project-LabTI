c9fc70b1444d721eb59f1a94ae67188226b9c5d2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 19:57:57 +0700

    tambah validasi dan authenticate pada model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates :name, :email, :password_hash, presence: true
+  validates :email, email_format: { message: 'Format email salah' }
+
+  def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    end
+  end
 end
