b176b0cfd867efcd2bc88366fb68446928046f48
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 22:40:00 +0700

    update student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,13 @@ class Student < ActiveRecord::Base
   before_create :set_role_enable
   validates :npm, :name, :class_name, :gender, :address, presence: true
 
+  def self.authenticate(npm, password)
+    student = Student.where(npm: npm).first
+    if student && student.password_hash == BCrypt::Engine.hash_secret(password, student.password_salt)
+      student
+    end
+  end
+
   private
 
   def set_role_enable
