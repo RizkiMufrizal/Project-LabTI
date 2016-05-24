62931caf3f9cf59a5404ce658b6d5b60ae3d4db1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-24 21:53:04 +0700

    tambah function crud untuk PracticumsController

--- app/controllers/practicums_controller.rb
+++ app/controllers/practicums_controller.rb
@@ -1,2 +1,47 @@
 class PracticumsController < ApplicationController
+  def index
+    @practicums = Practicum.all
+  end
+
+  def new
+    @practicum = Practicum.new
+  end
+
+  def create
+    @practicum = Practicum.new(practicum_params)
+    @practicum.id_practicum = SecureRandom.uuid
+    if @practicum.valid?
+      @practicum.save
+      flash[:notice] = 'data praktikum berhasil disimpan'
+      redirect_to practicums_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @practicum = Practicum.find(params[:id])
+  end
+
+  def update
+    @practicum = Practicum.find(params[:id])
+
+    if @practicum.update(practicum_params)
+      redirect_to practicums_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @practicum = Practicum.find(params[:id])
+    @practicum.destroy
+    redirect_to practicums_path
+  end
+
+  private
+
+  def practicum_params
+    params.require(:practicum).permit(:practicum_name)
+  end
 end
