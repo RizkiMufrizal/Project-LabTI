e2a4bbd3bbb25f3c81b1cb7e684e527b107fcfb5
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-26 18:31:36 +0700

    update model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
-  validates :name, :email, :password_hash, presence: true
-  validates :email, email_format: { message: 'Format email salah' }
+  before_create :ecrypt_password
+  validates :email, :password_hash, presence: true
 
   def self.authenticate(email, password)
     user = User.where(email: email).first
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     end
+  end
+
+  private
+
+  def ecrypt_password
+    self.role = 'ROLE_ADMIN'
+    self.enable = true
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password_hash, password_salt)
   end
 end
