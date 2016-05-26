9e881931657ccc01f223333903e3bd3000a196cb
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-26 16:25:08 +0700

    update hak akses students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,14 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    if session[:user_role]
+      if session[:user_role] == 'ROLE_ADMIN'
+        @students = Student.paginate(page: params[:page], per_page: 5)
+      else
+        redirect_to '/403'
+      end
+    else
+      redirect_to '/login'
+    end
   end
 
   def new
@@ -11,32 +19,82 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.valid?
       @student.save
-      flash[:notice] = 'anda berhasil melakukan register, silahkan login'
-      redirect_to '/login'
+      if session[:user_role]
+        flash[:notice] = 'anda berhasil membuat data mahasiswa'
+        redirect_to '/'
+      else
+        flash[:notice] = 'anda berhasil melakukan register, silahkan login'
+        redirect_to '/login'
+      end
     else
       render 'new'
     end
   end
 
   def edit
-    @student = Student.find(params[:id])
+    if session[:user_role]
+      if session[:user_role] == 'ROLE_ADMIN'
+        @student = Student.find(params[:id])
+      else
+        redirect_to '/403'
+      end
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def editBiodata
+    if session[:user_role]
+      if session[:user_role] == 'ROLE_MAHASISWA'
+        @student = Student.find(session[:user_role])
+      else
+        redirect_to '/403'
+      end
+    else
+      redirect_to '/login'
+    end
+  end
+
   end
 
   def update
-    @student = Student.find(params[:id])
-
-    if @student.update(student_params)
-      flash[:notice] = 'anda berhasil mengubah biodata anda'
-      redirect_to students_path
+    if session[:user_role]
+      if session[:user_role] == 'ROLE_ADMIN'
+        @student = Student.find(params[:id])
+        if @student.update(student_params)
+          flash[:notice] = 'data mahasiswa berhasil diubah'
+          redirect_to students_path
+        else
+          render 'edit'
+        end
+      elsif session[:user_role] == 'ROLE_MAHASISWA'
+        @student = Student.find(params[:id])
+        if @student.update(student_params)
+          flash[:notice] = 'anda berhasil mengubah biodata anda'
+          redirect_to '/'
+        else
+          render 'editBiodata'
+        end
+      else
+        redirect_to '/403'
+      end
     else
-      render 'edit'
+      redirect_to '/login'
     end
   end
 
   def destroy
-    @student = Student.find(params[:id])
-    @student.destroy
-    redirect_to students_path
+    if session[:user_role]
+      if session[:user_role] == 'ROLE_ADMIN'
+        @student = Student.find(params[:id])
+        @student.destroy
+        redirect_to students_path
+      else
+        redirect_to '/403'
+      end
+    else
+      redirect_to '/login'
+    end
   end
 
   private
