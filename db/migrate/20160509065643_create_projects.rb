f06febc16be028a771bb944fc698f840ce7b1f89
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-05-26 20:01:30 +0700

    update Migration projects

--- db/migrate/20160509065643_create_projects.rb
+++ db/migrate/20160509065643_create_projects.rb
@@ -3,7 +3,6 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects, id: false do |t|
       t.primary_key :id_project, :string, limit: 150
       t.string :name_project, limit: 50
-      t.string :responsible_person, limit: 50
       t.string :name_file, limit: 150
 
       t.timestamps null: false
